f3e716c6e9da5157bde0b3add2f076a12d9791c3
rishi-0205 <rishijc02@gmail.com>
2024-02-05 18:54:50 +0530

    Class Components

--- React-Practice/my-first-react-app/src/main.jsx
+++ React-Practice/my-first-react-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import Greeting from './Greeting.jsx'
 import './index.css'
+//import Clock from './clock.jsx'
+//import FunctionalInput from './funcCompo.jsx'
+import ClassInput from './classCompo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
+    <ClassInput />
   </React.StrictMode>,
 )
